fc318dafb40b23b3ed129e05deddb78b921e0933
MLL <baremetalcoder@gmail.com>
2021-03-01 15:35:34 -0800

    update flask service to support /time endpoint

--- app/src/server.py
+++ app/src/server.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from datetime import datetime
+
 server = Flask(__name__)
 
 
 @server.route("/")
-def baseUrl():
-    return "This is the base url."
+def home():
+    return "Flask Demo"
+
+
+@server.route("/time")
+def time():
+    now = datetime.now()
+    return now.strftime("%B %d, %Y at %X")
 
 
 if __name__ == "__main__":
